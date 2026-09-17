95e911d3602642c0db8535dc3d3a6e2e5ebd40cc
Reika-Baal <pratikdeuchand@gmail.com>
2026-09-17 13:35:21 +0100

    Getters and Setters created and ToString added.

--- src/main/java/com/pratik/jobtracker/model/Interview.java
+++ src/main/java/com/pratik/jobtracker/model/Interview.java
@@ -12,6 +12,66 @@ public class Interview {
     private String location;
     private String notes;
 
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getApplicationId() {
+        return applicationId;
+    }
+
+    public void setApplicationId(int applicationId) {
+        this.applicationId = applicationId;
+    }
+
+    public LocalDateTime getInterviewDateTime() {
+        return interviewDateTime;
+    }
+
+    public void setInterviewDateTime(LocalDateTime interviewDateTime) {
+        this.interviewDateTime = interviewDateTime;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public String getNotes() {
+        return notes;
+    }
+
+    public void setNotes(String notes) {
+        this.notes = notes;
+    }
+
+    @Override
+    public String toString() {
+        return "Interview{" +
+                "id=" + id +
+                ", applicationId=" + applicationId +
+                ", interviewDateTime=" + interviewDateTime +
+                ", type='" + type + '\'' +
+                ", location='" + location + '\'' +
+                ", notes='" + notes + '\'' +
+                '}';
+    }
+
     public Interview(
             int applicationId,
             LocalDateTime interviewDateTime,
