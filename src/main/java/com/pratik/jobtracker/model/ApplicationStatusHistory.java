434b0b7adda855b2ded46b1db37c35152f03cb3b
Reika-Baal <pratikdeuchand@gmail.com>
2026-09-19 19:20:32 +0100

    Getters and Setters created as well as toString.

--- src/main/java/com/pratik/jobtracker/model/ApplicationStatusHistory.java
+++ src/main/java/com/pratik/jobtracker/model/ApplicationStatusHistory.java
@@ -9,6 +9,48 @@ public class ApplicationStatusHistory {
     private ApplicationStatus status;
     private LocalDateTime reachedAt;
 
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getApplicationId() {
+        return applicationId;
+    }
+
+    @Override
+    public String toString() {
+        return "ApplicationStatusHistory{" +
+                "id=" + id +
+                ", applicationId=" + applicationId +
+                ", status=" + status +
+                ", reachedAt=" + reachedAt +
+                '}';
+    }
+
+    public void setApplicationId(int applicationId) {
+        this.applicationId = applicationId;
+    }
+
+    public ApplicationStatus getStatus() {
+        return status;
+    }
+
+    public void setStatus(ApplicationStatus status) {
+        this.status = status;
+    }
+
+    public LocalDateTime getReachedAt() {
+        return reachedAt;
+    }
+
+    public void setReachedAt(LocalDateTime reachedAt) {
+        this.reachedAt = reachedAt;
+    }
+
     public ApplicationStatusHistory(
             int applicationId,
             ApplicationStatus status,
